5986e05c1da4348adb32c24050e95acbcdb1d15b
N DIWAKAR <diwakarnath51@gmail.com>
2023-11-24 14:28:46 +0530

    v9

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,6 +5,7 @@ import ErrorHandler from "../utils/errorHandler.js";
 import sendToken from "../utils/sendToken.js";
 import sendEmail from "../utils/sendEmail.js";
 import crypto from "crypto";
+import e from "express";
 //register user
 
 export const registerUser = catchAsyncError(async (req, res) => {
@@ -134,3 +135,133 @@ export const resetPassword = catchAsyncError(async (req, res, next) => {
 
   sendToken(user, 200, res);
 });
+
+//get currently logged in user details
+
+export const getUserProfile = catchAsyncError(async (req, res, next) => {
+    const user = await User.findById(req?.user?.id);
+    
+    res.status(200).json({
+        success: true,
+        user,
+    });
+    }
+);
+
+//update password
+
+export const updatePassword = catchAsyncError(async (req, res, next) => {
+    const user = await User.findById(req?.user?.id).select("+password");
+    
+    //check previous user password
+    const isPasswordMatched = await user.comparePassword(req.body.oldPassword);
+    
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler("Old password is incorrect", 400));
+    }
+    
+    user.password = req.body.password;
+    await user.save();
+    
+    res.status(200).json({
+        success: true,
+        message: "Password updated successfully",
+    });
+    }
+);
+
+//update user profile
+
+export const updateProfile = catchAsyncError(async (req, res, next) => {
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email,
+    };
+    
+    //update avatar: TODO
+    
+    const user = await User.findByIdAndUpdate(req?.user?.id, newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+    });
+    
+    res.status(200).json({
+        success: true,
+        user
+    });
+    }
+);
+
+
+//get all users (admin)
+
+export const AllUsers = catchAsyncError(async (req, res, next) => {
+    const users = await User.find();
+    
+    res.status(200).json({
+        success: true,
+        users
+    });
+    }
+);
+
+//get user details (admin)
+
+export const getUserDetails = catchAsyncError(async (req, res, next) => {
+    const user = await User.findById(req?.params?.id);
+
+    if (!user) {
+        return next(new ErrorHandler(`User not found with id: ${req?.params?.id}`));
+    }
+
+    res.status(200).json({
+        success: true,
+        user
+    });
+    }   
+);
+
+//update user profile (admin)
+
+export const updateUser = catchAsyncError(async (req, res, next) => {
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email,
+        role: req.body.role,
+    };
+    
+    //update avatar: TODO
+    
+    const user = await User.findByIdAndUpdate(req?.params?.id, newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+    });
+    
+    res.status(200).json({
+        success: true,
+        user
+    });
+    }
+);
+
+//delete user (admin)
+
+export const deleteUser = catchAsyncError(async (req, res, next) => {
+    const user = await User.findById(req?.params?.id);
+
+    if (!user) {
+        return next(new ErrorHandler(`User not found with id: ${req?.params?.id}`));
+    }
+
+    //remove avatar from cloudinary: TODO
+
+    await user.deleteOne();
+
+    res.status(200).json({
+        success: true,
+        message: "User deleted successfully"
+    });
+    }
+);
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,6 @@
 import express from "express"
-import { forgotPassword, loginUser, logout, registerUser, resetPassword } from "../controllers/authController.js";
+import { AllUsers, deleteUser, forgotPassword, getUserDetails, getUserProfile, loginUser, logout, registerUser, resetPassword, updatePassword, updateProfile, updateUser } from "../controllers/authController.js";
+import { isAuthentiatedUser,authorizeRoles } from "../middleware/auth.js";
 
 const router = express.Router();
 
@@ -8,5 +9,11 @@ router.route("/login").post(loginUser)
 router.route("/logout").get(logout)
 router.route("/password/forgot").post(forgotPassword)
 router.route("/password/reset/:token").put(resetPassword)
+router.route("/me").get(isAuthentiatedUser,getUserProfile)
+router.route("/password/update").put(isAuthentiatedUser,updatePassword)
+router.route("/me/update").put(isAuthentiatedUser,updateProfile)
+router.route("/admin/users").get(isAuthentiatedUser,authorizeRoles("admin"),AllUsers)
+router.route("/admin/users/:id").get(isAuthentiatedUser,authorizeRoles("admin"),getUserDetails).put(isAuthentiatedUser,authorizeRoles("admin"),updateUser).delete(isAuthentiatedUser,authorizeRoles("admin"),deleteUser)
+
 
 export default router
