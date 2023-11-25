06c97f073f4f52fc1151a9033a373ba04a319f78
N DIWAKAR <diwakarnath51@gmail.com>
2023-11-25 20:03:59 +0530

    v13

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Footer from "./components/Layout/Footer";
 import Header from "./components/Layout/Header";
 import "./App.css";
 import Home from "./components/Home";
+import {Toaster} from 'react-hot-toast'
+import ProductDetails from "./components/product/ProductDetails";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Toaster position="top-center"/>
         <Header />
 
         <div className="container">
           <Routes>
             <Route path="/" element={<Home/>}/>
+            <Route path="/product/:id" element={<ProductDetails/>}/>
           </Routes>
         </div>
 
---
+++ frontend/src/redux/api/productsApi.js
@@ -0,0 +1,22 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+export const productApi = createApi({
+    reducerPath: "productApi",
+    baseQuery: fetchBaseQuery({baseUrl: "/api/v1"}),
+    keepUnusedDataFor:30,
+    endpoints: (builder) => ({
+        getProducts: builder.query({
+            query: (params) => ({
+                url: "/products",
+            })
+        }),
+
+        getProductDetails: builder.query({
+            query: (id) => ({
+                url: `/products/${id}`,
+            })
+        })
+    })
+})
+
+export const {useGetProductsQuery,useGetProductDetailsQuery} = productApi
